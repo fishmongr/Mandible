e2852e7005d0c034e2e3f6f77f0f8addddf60ece
Mark Ledford <mark@brainswap.me>
2012-01-02 18:14:13 -0500

    much improved remove-debug-code.sh console log strip regexp

--- bin/remove-debug-code.sh
+++ bin/remove-debug-code.sh
@@ -4,6 +4,6 @@ args = process.argv.splice(2);
 
 var file = args[0];
 var content = fs.readFileSync(file, 'utf8');
-content = content.replace(/[\s\t]*console.*/g, '');
+content = content.replace(/console.(log|debug|info|warn|error|assert)(.apply)?\(.*\);?/g, '');
 fs.writeFileSync(file, content, 'utf8');
 
